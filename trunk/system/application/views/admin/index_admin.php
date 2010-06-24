8a4b90ff9b0c428cec89a609633db514fde5b46d
daovanquyet <daovanquyet@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-06-24 11:00:21 +0000

    fix conflict with nam

--- trunk/system/application/views/admin/index_admin.php
+++ trunk/system/application/views/admin/index_admin.php
@@ -1,19 +1,25 @@
+<?php 
+    $admin_id=$this->session->userdata('admin_id');
+    $admin_right=(int)$this->session->userdata('right');  
+?>
 <div class="x2">
-  <h3>Profile administrator</h3>
-  <ul class="link_conttrol">
-    <li><a class="icon_list" href="<?php echo site_url('admin/list_admin')?>">Administrator List</a></li>
-    <li><a class="icon_add" href="<?php echo site_url('admin/add_new_admin')?>">New administrator</a></li>
-  </ul>
-  <p>&nbsp;</p>
+    <h3>Administrator Panel</h3>
+    <ul class="link_conttrol">
+    <?php if($admin_right==1){?>
+        <li><a class="icon_list" href="<?php echo site_url('admin/list_admin');?>">Administrator List</a></li>
+        <li><a class="icon_add" href="<?php echo site_url('admin/add_new_admin');?>">New administrator</a></li>
+    <?php }?>
+        <li><a class="icon_add" href="<?php echo site_url('admin/edit_profile/'.$admin_id);?>">Edit Profile</a></li>
+    </ul>
+    <p>&nbsp;</p>
 </div>
 <!-- /.x2 - represents a half windows size div -->
 
 <div class="x2 no_margin">
-  <h3>Speakers Control</h3>
-  <ul class="link_conttrol">
-    <li><a class="icon_list" href="<?php echo site_url('admin/list_user')?>">Speaker List</a></li>
-    <li><a class="icon_add" href="<?php echo site_url('admin/add_user')?>">Add new speaker</a></li>
-  </ul>
+    <h3>Speakers Control</h3>
+    <ul class="link_conttrol">
+        <li><a class="icon_list" href="<?php echo site_url('admin/list_user')?>">Speaker List</a></li>
+    </ul>
 </div>
 <div class="divider"></div>
 <div class="x2">
@@ -26,11 +32,11 @@
 <!-- /.x2 - represents a half windows size div -->
 
 <div class="x2 no_margin">
-  <h3>Event Control </h3>
-  <ul class="link_conttrol">
-    <li><a class="icon_list" href="<?php echo site_url('event/event_list')?>">Event List</a></li>
-    <li><a class="icon_add" href="<?php echo site_url('event/add_event')?>">New event</a></li>
-  </ul>
+    <h3>Event Control </h3>
+    <ul class="link_conttrol">
+        <li><a class="icon_list" href="<?php echo site_url('event/event_list')?>">Event List</a></li>
+        <li><a class="icon_add" href="<?php echo site_url('event/add_event')?>">New event</a></li>
+    </ul>
 </div>
 <div class="divider"></div>
 <div class="x2">
@@ -41,22 +47,22 @@
   </ul>
 </div>
 <div class="x2 no_margin">
-  <h3>Shop Control </h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta nibh metus. Aliquam eget tellus vulputate nibh feugiat dictum nec nec tellus. Nullam vel turpis sit amet erat cursus hendrerit in ut odio. Quisque aliquam, leo a ultricies pulvinar, mi erat ullamcorper quam, sit amet ornare erat lacus non nisl. Proin facilisis tempor lobortis. Sed magna eros, aliquam euismod fringilla quis, mattis et nisi.</p>
+    <h3>Shop Control </h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta nibh metus. Aliquam eget tellus vulputate nibh feugiat dictum nec nec tellus. Nullam vel turpis sit amet erat cursus hendrerit in ut odio. Quisque aliquam, leo a ultricies pulvinar, mi erat ullamcorper quam, sit amet ornare erat lacus non nisl. Proin facilisis tempor lobortis. Sed magna eros, aliquam euismod fringilla quis, mattis et nisi.</p>
 </div>
 <div class="divider"></div>
 <div class="x2">
-  <h3>Blog Control</h3>
-  <ul class="link_conttrol">
-    <li><a class="icon_list" href="<?php echo site_url('blog/blog_list')?>">Blog List</a></li>
-    <li><a class="icon_add" href="<?php echo site_url('blog/add_blog')?>">New blog</a></li>
-  </ul>
+    <h3>Blog Control</h3>
+    <ul class="link_conttrol">
+        <li><a class="icon_list" href="<?php echo site_url('blog/blog_list')?>">Blog List</a></li>
+        <li><a class="icon_add" href="<?php echo site_url('blog/add_blog')?>">New blog</a></li>
+    </ul>
 </div>
 <div class="x2 no_margin">
-  <h3>Paying advertisements Control </h3>
-  <ul class="link_conttrol">
-    <li><a class="icon_list" href="<?php echo site_url('advertisement/advertisement_list')?>">Advertisement List</a></li>
-    <li><a class="icon_add" href="<?php echo site_url('advertisement/add')?>">New Advertisement</a></li>
-  </ul>
+    <h3>Paying advertisements Control </h3>
+    <ul class="link_conttrol">
+        <li><a class="icon_list" href="<?php echo site_url('advertisement/advertisement_list')?>">Advertisement List</a></li>
+        <li><a class="icon_add" href="<?php echo site_url('advertisement/add')?>">New Advertisement</a></li>
+    </ul>
 </div>
 <div class="divider"></div>
