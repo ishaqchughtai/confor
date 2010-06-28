479b4935f650d5e79dbad97bc429707c615c91e0
yugi3000 <yugi3000@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-06-28 08:21:25 +0000

    sua loi short tag php <?=  --> <?php echo

--- system/application/views/speaker/user_login.php
+++ system/application/views/speaker/user_login.php
@@ -19,23 +19,23 @@
 ?>
 
 		<h2>SPEAKER LOGIN</h2>
-		<?=form_open('home/login');?>
+		<?php echo form_open('home/login');?>
 		<fieldset class="login">
 			<legend>Login as a speaker</legend>		
 			<?php if(isset($error)):?>
 				<p class="not_error medium"><span class="img"></span><?php echo $error;?><span class="close"></span></p>			
-			<?endif;?>			
+			<?php endif;?>			
 			<div class="input text"><label>Username</label>		
-				<?=form_input($usernameInput);?>
+				<?php echo form_input($usernameInput);?>
 			</div>					
-			<?=form_error('username');?>
+			<?php echo form_error('username');?>
 			<div class="input text"><label>Password</label>
-				<?=form_input($passwordInput);?>			
+				<?php echo form_input($passwordInput);?>			
 			</div>					
-			<?=form_error('password');?>
+			<?php echo form_error('password');?>
 			<div class="input text">
 				<p class="not_login_info">
-					<?=form_input($rememberInput);?>Remember me.					
+					<?php echo form_input($rememberInput);?>Remember me.					
 					<a href="#">Lost your password or username?</a>					
 				</p>
 			</div>
