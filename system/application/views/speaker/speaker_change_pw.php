a14de06b7fc8c47726119d76f1816c4e4cb997f6
yugi3000 <yugi3000@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-06-29 03:52:26 +0000

    php short tag, change password view

--- system/application/views/speaker/speaker_change_pw.php
+++ system/application/views/speaker/speaker_change_pw.php
@@ -20,22 +20,22 @@
 	);			
 ?>
 		<h2>CHANGE PASSWORD</h2>
-		<?=form_open('speaker/change_password');?>
+		<?php echo form_open('speaker/change_password');?>
 		<fieldset class="change_pw">
 			<legend>Change your password</legend>			
 			<?php if(isset($error)):?>
 				<p class="not_error medium"><span class="img"></span><?php echo $error;?><span class="close"></span></p>			
 			<?php endif;?>
 			<div class="input text"><label>Old password</label>
-				<?=form_input($oldPasswordInput);?>
+				<?php echo form_input($oldPasswordInput);?>
 			</div>			
 			<?php echo form_error('old_password'); ?>			
 			<div class="input text"><label>New password</label>
-				<?=form_input($newPasswordInput);?>
+				<?php echo form_input($newPasswordInput);?>
 			</div>			
 			<?php echo form_error('new_password'); ?>
 			<div class="input text"><label>Confirm new password</label>
-				<?=form_input($passwordfInput);?>
+				<?php echo form_input($passwordfInput);?>
 			</div>			
 			<?php echo form_error('passconf'); ?>
 			<input type="submit" class="submit" name="change" value="Change password">			
