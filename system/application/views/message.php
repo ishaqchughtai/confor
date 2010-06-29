770e79c7c1f44af23124f8297483767403ab69d0
yugi3000 <yugi3000@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-06-29 11:10:20 +0000

    Check membership , display subcription infor

--- system/application/views/message.php
+++ system/application/views/message.php
@@ -1,7 +1,10 @@
 <?php
 	$msg = $this->session->flashdata('msg');
 	$class_msg = $this->session->flashdata('class_msg');
-	$msg = isset($msg)?$msg:'{No message here}';
-	$class_msg = isset($class_msg)?$class_msg:'warning';	
+	// $msg = isset($msg)?$msg:'{No message here}';
+	// $class_msg = isset($class_msg)?$class_msg:'warning';	
 ?>
+
+<?php if ( $msg && $class_msg):?>
 <p class="not_<?php echo $class_msg?>"><span class="img"></span><?php echo $msg?><span class="close"></span></p>
+<?php endif;?>
