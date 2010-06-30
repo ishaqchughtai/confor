27b4fc231d6a6a3b604fe5b1148c2e02d3f46a48
yugi3000 <yugi3000@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-06-30 18:24:34 +0000

    fix video list link

--- trunk/system/application/views/admin/index_admin.php
+++ trunk/system/application/views/admin/index_admin.php
@@ -27,7 +27,7 @@
     <h3><?php echo __("CF_vid_con")?></h3>                                                                                      
     <ul class="link_conttrol">
         <li><a class="icon_list" href="<?php echo site_url('showroom/add_new_images')?>"><?php echo __("CF_showroom")?></a></li>  
-        <li><a class="icon_list" href="<?php echo site_url('admin/list_video_conference/0/3')?>"><?php echo __("CF_admin_vid_list")?></a></li>
+        <li><a class="icon_list" href="<?php echo site_url('admin/list_video_conference')?>"><?php echo __("CF_admin_vid_list")?></a></li>
         <li><a class="icon_add" href="<?php echo site_url('admin/new_video_conference')?>"><?php echo __("CF_admin_new_vid")?></a></li>
     </ul>
 </div>
