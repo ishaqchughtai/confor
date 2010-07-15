44c4dacf1a4c3ccacdbea928806f1d8d3cf11bf9
daovanquyet <daovanquyet@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-07-15 04:52:12 +0000

    tempo view conference_office.php

--- system/application/controllers/conference_office.php
+++ system/application/controllers/conference_office.php
@@ -18,25 +18,8 @@
 
         function index()
         {              
-            $this->_data['video_path'] = $this->Mhome->get_top_viewed_video();        
-            $this->_data['page_title'] = 'Video Conference';
-            
-            $config['base_url'] = base_url().'index.php/conference_office/index/';
-            $config['total_rows'] = $this->db->count_all('videos');
-            $config['per_page']= $this->per_page;
-
-            $config['full_tag_open'] = '<li>';        
-            $config['full_tag_close'] = '</li>'; 
-            $config['next_link'] = 'Next >';
-            $config['prev_link'] = '< Previous';
-            $config['last_link'] = 'Last >>';
-            $config['first_link'] = '<< First';
-            
-            $this->pagination->initialize($config);
-            $offset = $this->uri->segment(3);
-            $this->_data['conference_list'] = $this->Mcoffice->list_conference_office($offset,$config['per_page']);
-            $this->_data['pagination'] = $this->pagination->create_links();                
-            $this->_load_view('conference_office/conference'); 
+            $this->_data['page_title'] = 'Conference Office';                
+            $this->_load_view('conference_office/conference_office_view'); 
         }
     }
 ?>
