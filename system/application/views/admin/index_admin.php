b3f9bb55316330d1780784da7de0d34121ab7f99
yugi3000 <yugi3000@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-07-16 14:32:48 +0000

    update language for video_setting(index_admin)

--- system/application/views/admin/index_admin.php
+++ system/application/views/admin/index_admin.php
@@ -44,7 +44,7 @@
     <ul class="link_conttrol">
         <li><a class="icon_image" href="<?php echo site_url('showroom/list_images')?>"><?php echo __("CF_showroom")?></a></li>  
         <li><a class="icon_add_category" href="<?php echo site_url('admin/category_managing')?>"><?php echo __("CF_cate_man")?></a></li>
-        <li><a class="icon_setting" href="<?php echo site_url('vid/setting')?>">Video settings</a></li>
+        <li><a class="icon_setting" href="<?php echo site_url('vid/setting')?>"><?php echo __("CF_vid_conf")?></a></li>
         <li><a class="icon_list" href="<?php echo site_url('vid/list_video_conference/')?>/<?php echo $lang?>"><?php echo __("CF_admin_vid_list")?></a></li>
         <li><a class="icon_add" href="<?php echo site_url('vid/new_video_conference')?>"><?php echo __("CF_admin_new_vid")?></a></li>		
     </ul>	    
