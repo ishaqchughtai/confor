814da4caf2f7fc052e367095a65d52b043da940a
yugi3000 <yugi3000@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-07-19 03:51:11 +0000

    rss blog update

--- trunk/system/application/views/home/rss_blog.php
+++ trunk/system/application/views/home/rss_blog.php
@@ -1,20 +1,21 @@
-<?php 
+<?php
 echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 ?>
 <rss version="2.0">
-    <channel>    
+    <channel>
     <title><?php echo $feed_name; ?></title>
+	<description><![CDATA[ <?php echo $page_description;?> ]]></description>
     <link><?php echo $feed_url; ?></link>
 	<copyright>Confor.ca</copyright>
-    <description><![CDATA[ <?php echo $page_description;?> ]]></description>
-    
+	<pubDate><?php echo gmdate("D, d M Y H:i:s \G\M\T", time());?></pubDate>
+    <lastBuildDate><?php echo gmdate("D, d M Y H:i:s \G\M\T", time());?></lastBuildDate>
     <?php foreach($blogs->result() as $item): ?>
         <item>
-          <title><?php echo xml_convert($item->Title); ?></title>		  
-          <link><?php echo site_url('blog_frontend/blog_content/' . $item->Name.'/'.$item->Title) ?></link>		            
-          <description><![CDATA[ <?php echo character_limiter(strip_tags($item->Text),200);?> ]]></description>
-	      <pubDate><?php echo $item->Date;?></pubDate>
-        </item>        
+          <title><?php echo xml_convert($item->Title); ?></title>
+		  <description><![CDATA[ <?php echo character_limiter(strip_tags($item->Text),200);?> ]]></description>
+          <link><?php echo site_url('blog_frontend/blog_content/' . $item->Name.'/'.$item->Title);?></link>
+	      <pubDate><?php echo gmdate("D, d M Y H:i:s \G\M\T",strtotime($item->Date));?></pubDate>
+        </item>
     <?php endforeach; ?>
-    </channel>	
-</rss>  
+    </channel>
+</rss>
