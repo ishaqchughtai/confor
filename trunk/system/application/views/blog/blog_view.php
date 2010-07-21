68841e5f17fdff40e09827e6b419b5727ef58eba
huunam09 <huunam09@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-07-21 11:40:48 +0000

    update blog !

--- trunk/system/application/views/blog/blog_view.php
+++ trunk/system/application/views/blog/blog_view.php
@@ -12,8 +12,24 @@
         $author      =$row['Name'];
         $date        =$row['Date'];
         $countcomment=$row['CountComment'];
+        $is_admin = $this->session->userdata('admin');
     ?>
     <div class = "content_item">
+    <?php if($is_admin):?>
+        <h3><a href = "<?php echo site_url('blog_frontend/blog_content_admin/'.$author.'/'.$title);?>"><?php echo $title; ?></a></h3> 
+        <h5><?php echo $subject; ?></h5>
+        <a href = "<?php echo site_url('blog_frontend/blog_content_admin/'.$author.'/'.$title)?>#comments" class = "comments_balloon"><?php echo $countcomment; ?></a>
+
+        <p>
+            <img src = "<?php echo base_url().'assets/uploads/image/'.$link ?>" width = "610" height = "169" alt = "Confor.ca"/>
+        </p>
+
+        <p><?php echo word_limiter(strip_tags($text), 100); ?></p>
+
+        <a href = "<?php echo site_url('blog_frontend/blog_content_admin/'.$author.'/'.$title);?>" class = "read_more">
+            <?php echo __("CF_continue"); ?>
+        </a>
+    <?php else:?>
         <h3><a href = "<?php echo site_url('blog_frontend/blog_content/'.$author.'/'.$title);?>"><?php echo $title; ?></a></h3> 
         <h5><?php echo $subject; ?></h5>
         <a href = "<?php echo site_url('blog_frontend/blog_content/'.$author.'/'.$title)?>#comments" class = "comments_balloon"><?php echo $countcomment; ?></a>
@@ -26,7 +42,8 @@
 
         <a href = "<?php echo site_url('blog_frontend/blog_content/'.$author.'/'.$title);?>" class = "read_more">
             <?php echo __("CF_continue"); ?>
-        </a>
+        </a>    
+    <?php endif;?> 
     </div>
     <?php
     }
@@ -52,7 +69,11 @@
     ?>
 
     <div class = "content_item">
+    <?php if($is_admin):?>
+        <h3><a href = "<?php echo site_url('blog_frontend/blog_content_admin/'.$author.'/'.$title); ?>"><?php echo $title ?></a></h3>
+    <?php else:?>
         <h3><a href = "<?php echo site_url('blog_frontend/blog_content/'.$author.'/'.$title); ?>"><?php echo $title ?></a></h3>
+    <?php endif;?>
         <h5><?php echo __("CF_by") ?> <?php echo $author ?> <?php echo __("CF_in") ?> : 
             <?php
                 for ($i=0; $i < count($key); $i++)
@@ -66,8 +87,11 @@
             ?>
             // <?php echo $final_date ?> 
         </h5>
-        <a href = "<?php echo site_url('blog_frontend/blog_content/'.$author.'/'.$title)?>#comments" class = "comments_balloon"><?php echo $countcomment ?></a>
-
+        <?php if($is_admin):?>
+            <a href = "<?php echo site_url('blog_frontend/blog_content_admin/'.$author.'/'.$title)?>#comments" class = "comments_balloon"><?php echo $countcomment ?></a>
+        <?php else:?>
+            <a href = "<?php echo site_url('blog_frontend/blog_content/'.$author.'/'.$title)?>#comments" class = "comments_balloon"><?php echo $countcomment ?></a>
+        <?php endif;?>
         <p>
             <img src = "<?php echo base_url().'assets/uploads/image/'.$link ?>" width = "202" height = "90" alt = "Confor.ca" class = "alignleft"/>
         </p>
@@ -75,8 +99,11 @@
         <p>
             <?php echo word_limiter(strip_tags($text), 100); ?>
         </p>    
-
-        <a href = "<?php echo site_url('blog_frontend/blog_content/'.$author.'/'.$title); ?>" class = "read_more"><?php echo __("CF_continue") ?></a>
+        <?php if($is_admin):?> 
+            <a href = "<?php echo site_url('blog_frontend/blog_content_admin/'.$author.'/'.$title); ?>" class = "read_more"><?php echo __("CF_continue") ?></a>
+        <?php else:?>
+            <a href = "<?php echo site_url('blog_frontend/blog_content/'.$author.'/'.$title); ?>" class = "read_more"><?php echo __("CF_continue") ?></a>
+        <?php endif;?>
     </div>
 
     <?php
