ec841ee21b5839450f03397a0135795164599c88
huunam09 <huunam09@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-08-04 09:46:17 +0000

    them chuc nang quan ly spam email

---
+++ trunk/system/application/views/spamemail/spam_email_view.php
@@ -0,0 +1,30 @@
+<?php $this->load->view("spamemail/admin_menu_spam_menu.php");?>  
+  <!-- /.content_item -->
+<div class="content_item">     
+    <table width="100%" border="1">
+        <tr align="left">
+            <th width="90%">Email</th>       
+            <th width="10%"><?php echo __("CF_action")?></th>
+        </tr>
+                <?php foreach($query as $row){                               
+                                $id = $row['ID'];
+                                $Email = $row['Email'];                                                           
+        ?>    
+            <tr>
+                <td><?php echo $Email?></td>
+                <td>
+                    <li style="list-style-type:none">
+                        <div align="center"><label><a href="<?php echo site_url('spamemail/delete_spam_email').'/'.$id?>"onclick="return confirm('<?php echo __("CF_mess_delete")?>')"><?php echo __("CF_remove")?></a></label></div>
+                    </li>
+                </td> 
+            </tr>
+            <?php }?>
+        <tr>
+            <td colspan="8" valign="center">               
+                <ul id="pagination">
+                    <?php echo $pagination?>
+                </ul>
+            </td>
+        </tr> 
+    </table>
+</div>
