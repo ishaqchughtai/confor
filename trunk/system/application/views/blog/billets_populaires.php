fa78761291bd03f36dec05824d3966b6690d2b61
huunam09 <huunam09@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-08-11 08:40:38 +0000

    update billets_populaires va popular_posts

--- trunk/system/application/views/blog/billets_populaires.php
+++ trunk/system/application/views/blog/billets_populaires.php
@@ -72,6 +72,74 @@
         <!-- /.popular_posts_content -->
 
     </li>
+    <!-- /.popular_posts li - popular posts slider item --><li>
+
+        <ul class="popular_posts_content">
+            <?php
+                $total = (count($bill)-1>11)?11:count($bill)-1;
+                for($i=8;$i<=$total;$i++){
+                    $row = $bill[$i];                               
+                    $link = $row['Link'];
+                    $title = $row['Title'];
+                    $CountView = $row['CountView'];
+                    $countcomment = $row['CountComment'];
+                    $author = $row['Name']; 
+                    $id=$row['ID'];                                
+                ?>
+                <li>
+                    <?php if($is_admin):?>                                
+                        <img src="<?php echo base_url().'assets/uploads/image/thumb_'.$link?>" width="60" height="60" alt="Popular Post" class="alignleft" />
+
+                        <a href="<?php echo site_url('blog_frontend/blog_content_admin'.'/'.$author.'/'.$id.'/'.$title); ?>" class="title"><?php echo $title?></a>
+                        <a href="<?php echo site_url('blog_frontend/blog_content_admin'.'/'.$author.'/'.$id.'/'.$title); ?>#comments"><?php echo $countcomment?> <?php echo __("CF_cm")?></a>
+                        <?php else:?>
+                        <img src="<?php echo base_url().'assets/uploads/image/thumb_'.$link?>" width="60" height="60" alt="Popular Post" class="alignleft" />
+
+                        <a href="<?php echo site_url('blog_frontend/blog_content'.'/'.$author.'/'.$id.'/'.$title); ?>" class="title"><?php echo $title?></a>
+                        <a href="<?php echo site_url('blog_frontend/blog_content'.'/'.$author.'/'.$id.'/'.$title); ?>#comments"><?php echo $countcomment?> <?php echo __("CF_cm")?></a>
+                        <?php endif;?>
+                </li>
+                <?php }?>
+            <!-- /.popular_posts_content li -->
+        </ul>
+        <!-- /.popular_posts_content -->
+
+    </li>
+    <!-- /.popular_posts li - popular posts slider item -->
+    
+    <li>
+
+        <ul class="popular_posts_content">
+            <?php
+                $total = (count($bill)-1>15)?15:count($bill)-1;
+                for($i=12;$i<=$total;$i++){
+                    $row = $bill[$i];                               
+                    $link = $row['Link'];
+                    $title = $row['Title'];
+                    $CountView = $row['CountView'];
+                    $countcomment = $row['CountComment'];
+                    $author = $row['Name']; 
+                    $id=$row['ID'];                                
+                ?>
+                <li>
+                    <?php if($is_admin):?>                                
+                        <img src="<?php echo base_url().'assets/uploads/image/thumb_'.$link?>" width="60" height="60" alt="Popular Post" class="alignleft" />
+
+                        <a href="<?php echo site_url('blog_frontend/blog_content_admin'.'/'.$author.'/'.$id.'/'.$title); ?>" class="title"><?php echo $title?></a>
+                        <a href="<?php echo site_url('blog_frontend/blog_content_admin'.'/'.$author.'/'.$id.'/'.$title); ?>#comments"><?php echo $countcomment?> <?php echo __("CF_cm")?></a>
+                        <?php else:?>
+                        <img src="<?php echo base_url().'assets/uploads/image/thumb_'.$link?>" width="60" height="60" alt="Popular Post" class="alignleft" />
+
+                        <a href="<?php echo site_url('blog_frontend/blog_content'.'/'.$author.'/'.$id.'/'.$title); ?>" class="title"><?php echo $title?></a>
+                        <a href="<?php echo site_url('blog_frontend/blog_content'.'/'.$author.'/'.$id.'/'.$title); ?>#comments"><?php echo $countcomment?> <?php echo __("CF_cm")?></a>
+                        <?php endif;?>
+                </li>
+                <?php }?>
+            <!-- /.popular_posts_content li -->
+        </ul>
+        <!-- /.popular_posts_content -->
+
+    </li>
     <!-- /.popular_posts li - popular posts slider item -->
 
-                        </ul>
+</ul>
