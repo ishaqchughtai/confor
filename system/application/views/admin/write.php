0a7a1d90bf6f58f9c412cd8d5f089bfe6c0e28f8
cuongdt90 <cuongdt90@gmail.com>
2010-08-27 16:24:32 +0000

    update new videositemap

--- system/application/views/admin/write.php
+++ system/application/views/admin/write.php
@@ -21,6 +21,7 @@
   $r->setAttributeNS('http://www.w3.org/2000/xmlns/','xmlns:video','http://www.google.com/schemas/sitemap-video/1.0');
   foreach( $query as $row )
   {
+  $keyword = explode(" ", $row['tags']);
   
   $b = $doc->createElement( "url" );
   
@@ -31,6 +32,12 @@
   $b->appendChild( $id );
   
   $video = $doc->createElement("video:video");
+  
+  $thumbnail = $doc->createElement( "video:thumbnail_loc" );
+  $thumbnail->appendChild(
+  $doc->createTextNode('http://confor.tv/thumbs/'.$row['shash'])
+  );
+  $video->appendChild( $thumbnail );
   
   $title = $doc->createElement( "video:title" );
   $title->appendChild(
@@ -43,6 +50,40 @@
   $doc->createTextNode($row['description'])
   );
   $video->appendChild( $description );
+  
+  $viewed = $doc->createElement( "video:view_count" );
+  $viewed->appendChild(
+  $doc->createTextNode($row['viewed'])
+  );
+  $video->appendChild( $viewed );
+  
+  for($i=0;$i<count($keyword);$i++)
+  {
+	  $tags = $doc->createElement( "video:tag" );
+	  $tags->appendChild(
+	  $doc->createTextNode($keyword[$i])
+	  );
+	  $video->appendChild( $tags );
+  }
+  
+  $date = $doc->createElement( "video:publication_date" );
+  $date->appendChild(
+  $doc->createTextNode(mdate("%Y-%m-%d", $row['date']))
+  );
+  $video->appendChild( $date );
+  
+  $category = $doc->createElement( "video:category" );
+  $category->appendChild(
+  $doc->createTextNode($row['Name'])
+  );
+  $video->appendChild( $category );
+  
+  $uploader = $doc->createElement( "video:uploader" );
+  $uploader->appendChild(
+  $doc->createTextNode($row['first_name'].$row['name'])
+  );
+  $video->appendChild( $uploader );
+  
   $b->appendChild( $video );
   $r->appendChild( $b );
   }
--- system/application/views/admin/writexml.php
+++ system/application/views/admin/writexml.php
@@ -3,11 +3,20 @@ echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
 ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:video="http://www.google.com/schemas/sitemap-video/1.0">
 <?php foreach( $query as $row ):?>
+<?php $keyword = explode(" ", $row['tags']);?>
   <url>
     <loc><?php echo 'http://confor.tv/index.php/video/play/'.$row['vid_id']?></loc>
     <video:video>
+      <video:thumbnail_loc><?php echo base_url().'thumbs/'.$row['shash'];?></video:thumbnail_loc>
       <video:title><![CDATA[<?php echo $row['title']?>]]></video:title>
       <video:description><![CDATA[<?php echo $row['description']?>]]></video:description>
+      <video:view_count><?php echo $row['viewed']?></video:view_count>
+      <?php for($i=0;$i<count($keyword);$i++):?>
+      <video:tag><![CDATA[<?php echo $keyword[$i]?>]]></video:tag>
+      <?php endfor?>
+      <video:publication_date><?php echo mdate("%Y-%m-%d", $row['date'])?></video:publication_date>
+      <video:category><![CDATA[<?php echo $row['Name']?>]]></video:category>
+      <video:uploader><![CDATA[<?php echo $row['first_name'].$row['name']?>]]></video:uploader>
     </video:video>
   </url>
   <?php endforeach;?>
