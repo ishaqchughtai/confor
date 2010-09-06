66974f92972a74866ec58fdc254a2a472d588331
yugi3000 <yugi3000@7f519575-8b79-c1f5-dad8-5345b0047163>
2010-09-06 08:53:27 +0000

    SEO link alt title ....... (main content)

--- trunk/system/application/views/home/get_video_by_category.php
+++ trunk/system/application/views/home/get_video_by_category.php
@@ -4,7 +4,7 @@
     <?php $keyword = explode(" ", $rows['tags']);?>
     <div class="content_item">
 
-        <h3><a href="<?php echo site_url('video/play').'/'.$rows['vid_id']?>"><?php echo $rows['title'];?></a></h3>
+        <h3><a alt="<?php echo $rows['title'].' - '.$rows['first_name'].' '.$rows['name']?>" title="<?php echo $rows['title'].' - '.$rows['first_name'].' '.$rows['name']?>" href="<?php echo site_url('video/play').'/'.$rows['vid_id']?>"><?php echo $rows['title'];?></a></h3>
 
         <h5><?php echo __("CF_by")?> 
             <a href="<?php echo site_url('video/speaker').'/'.$rows['mem_id'].'/'.$page?>"><b><?php echo $rows['first_name'].' '.$rows['name'] ?></b></a> 
@@ -16,7 +16,7 @@
 
             // <?php echo mdate("%d-%m-%Y", $rows['date'])?></h5>                     				
     <p>
-        <a href="<?php echo site_url('video/play').'/'.$rows['vid_id']?>"><img src="<?php echo base_url().'thumbs/'.$rows['shash']?>" width="160" height="90" alt="" class="alignleft" /></a>
+        <a href="<?php echo site_url('video/play').'/'.$rows['vid_id']?>"><img alt="<?php echo $rows['title'].' - '.$rows['first_name'].' '.$rows['name']?>" title="<?php echo $rows['title'].' - '.$rows['first_name'].' '.$rows['name']?>" src="<?php echo base_url().'thumbs/'.$rows['shash']?>" width="160" height="90" alt="" class="alignleft" /></a>
     </p>
 
     <p>
